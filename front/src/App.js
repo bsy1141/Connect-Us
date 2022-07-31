b4fbc948b0d9454b5670ab22dd27d14154a39325
bsy1141 <bsy1141@naver.com>
2022-08-01 01:25:29 +0900

    feat: 파일 wrapper+리듀서 컨텍스트 분리로 인한 코드 수정

--- front/src/App.js
+++ front/src/App.js
@@ -1,8 +1,6 @@
-import React, { useState, useEffect, useReducer, createContext } from "react";
+import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
-import * as Api from "./api";
-import { loginReducer } from "./reducer";
 import styled from "styled-components";
 import "./style/reset.css";
 
@@ -14,69 +12,39 @@ import SearchPage from "./pages/search/SearchPage";
 import AddPostPage from "./pages/post/AddPostPage";
 import PostPage from "./pages/post/PostPage";
 import KeywordPage from "./pages/user/keyword/KeywordPage";
-
-export const UserStateContext = createContext(null);
-export const DispatchContext = createContext(null);
+import ContextProvider from "./components/ContextProvider";
+import FetchCurrentUser from "./components/FetchCurrentUser";
 
 function App() {
-  // useReducer 훅을 통해 userState 상태와 dispatch함수를 생성함.
-  const [userState, dispatch] = useReducer(loginReducer, {
-    user: null,
-  });
-
-  // 아래의 fetchCurrentUser 함수가 실행된 다음에 컴포넌트가 구현되도록 함.
-  // 아래 코드를 보면 isFetchCompleted 가 true여야 컴포넌트가 구현됨.
-  const [isFetchCompleted, setIsFetchCompleted] = useState(false);
-
-  const fetchCurrentUser = async () => {
-    try {
-      // 이전에 발급받은 토큰이 있다면, 이를 가지고 유저 정보를 받아옴.
-      const res = await Api.get("user/current");
-      const currentUser = res.data;
-
-      // dispatch 함수를 통해 로그인 성공 상태로 만듦.
-      dispatch({
-        type: "LOGIN_SUCCESS",
-        payload: currentUser,
-      });
-
-      console.log("%c sessionStorage에 토큰 있음.", "color: #d93d1a;");
-    } catch {
-      console.log("%c SessionStorage에 토큰 없음.", "color: #d93d1a;");
-    }
-    // fetchCurrentUser 과정이 끝났으므로, isFetchCompleted 상태를 true로 바꿔줌
-    setIsFetchCompleted(true);
+  const wrapFetchUser = (child) => {
+    return <FetchCurrentUser>{child}</FetchCurrentUser>;
   };
 
-  // useEffect함수를 통해 fetchCurrentUser 함수를 실행함.
-  useEffect(() => {
-    fetchCurrentUser();
-  }, []);
-
-  if (!isFetchCompleted) {
-    return "loading...";
-  }
+  const mainfeed = wrapFetchUser(<MainFeed />);
+  const addpostPage = wrapFetchUser(<AddPostPage />);
+  const postPage = wrapFetchUser(<PostPage />);
+  const myPage = wrapFetchUser(<MyPage />);
+  const keywordPage = wrapFetchUser(<KeywordPage />);
+  const searchPage = wrapFetchUser(<SearchPage />);
 
   return (
-    <DispatchContext.Provider value={dispatch}>
-      <UserStateContext.Provider value={userState}>
-        <Router>
-          <Container>
-            <Routes>
-              <Route path="/" exact element={<MainFeed />} />
-              <Route path="/login" element={<LoginForm />} />
-              <Route path="/register" element={<RegisterForm />} />
-              <Route path="/post" element={<AddPostPage />} />
-              <Route path="/post/:id" element={<PostPage />} />
-              <Route path="/users/:userId" element={<MyPage />} />
-              <Route path="/keyword" element={<KeywordPage />} />
-              <Route path="/search" element={<SearchPage />} />
-              <Route path="*" element={<MainFeed />} />
-            </Routes>
-          </Container>
-        </Router>
-      </UserStateContext.Provider>
-    </DispatchContext.Provider>
+    <ContextProvider>
+      <Router>
+        <Container>
+          <Routes>
+            <Route path="/" exact element={mainfeed} />
+            <Route path="/login" element={<LoginForm />} />
+            <Route path="/register" element={<RegisterForm />} />
+            <Route path="/post" element={addpostPage} />
+            <Route path="/post/:id" element={postPage} />
+            <Route path="/users/:userId" element={myPage} />
+            <Route path="/keyword" element={keywordPage} />
+            <Route path="/search" element={searchPage} />
+            <Route path="*" element={mainfeed} />
+          </Routes>
+        </Container>
+      </Router>
+    </ContextProvider>
   );
 }
 
