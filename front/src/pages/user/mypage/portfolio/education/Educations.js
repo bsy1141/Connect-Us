d41788b59664b1cd6826a910b066667a3cb1ee32
bsy1141 <bsy1141@naver.com>
2022-08-02 00:01:22 +0900

    docs: 불필요한 주석 제거

--- front/src/pages/user/mypage/portfolio/education/Educations.js
+++ front/src/pages/user/mypage/portfolio/education/Educations.js
@@ -1,4 +1,4 @@
-import { useState, useEffect, useContext } from "react";
+import { useState, useEffect } from "react";
 import styled from "styled-components";
 import * as Api from "api";
 import EducationCard from "./EducationCard";
