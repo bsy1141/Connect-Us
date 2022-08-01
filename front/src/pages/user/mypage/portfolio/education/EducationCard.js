4964bd3f16fbc1dac07e39382c81058bf3b11bed
bsy1141 <bsy1141@naver.com>
2022-08-01 23:02:13 +0900

    style: 학력 카드 스타일링 완료

--- front/src/pages/user/mypage/portfolio/education/EducationCard.js
+++ front/src/pages/user/mypage/portfolio/education/EducationCard.js
@@ -16,16 +16,18 @@ const EducationCard = ({ userId, education, setEducations, isEditable }) => {
         />
       ) : (
         <CardContainer>
-          <Date>
-            <h3>
-              {education.admission} ~ {education.graduate}
-            </h3>
-            <p>{education.position}</p>
-          </Date>
-          <Content>
-            <h3>{education.school}</h3>
-            <p>{education.major}</p>
-          </Content>
+          <CardWrapper>
+            <Date>
+              <h3>
+                {education.admission} ~ {education.graduate}
+              </h3>
+              <p>{education.position}</p>
+            </Date>
+            <Content>
+              <h3>{education.school}</h3>
+              <p>{education.major}</p>
+            </Content>
+          </CardWrapper>
           {isEditable && (
             <Buttons>
               <button onClick={() => setIsEditing(true)}>편집</button>
@@ -40,10 +42,48 @@ const EducationCard = ({ userId, education, setEducations, isEditable }) => {
 
 export default EducationCard;
 
-const CardContainer = styled.div``;
+const CardContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+`;
 
-const Date = styled.div``;
+const CardWrapper = styled.div`
+  display: flex;
+  width: 80%;
+`;
 
-const Content = styled.div``;
+const Date = styled.div`
+  > h3 {
+    font-size: 15px;
+    font-weight: normal;
+  }
+  > p {
+    font-size: 15px;
+    color: #3399ff;
+  }
+`;
 
-const Buttons = styled.div``;
+const Content = styled.div`
+  margin-left: 30px;
+  > h3 {
+    display: inline-block;
+    font-size: 18px;
+  }
+  > p {
+    display: inline-block;
+    margin-left: 10px;
+    font-size: 15px;
+    color: #666;
+  }
+`;
+
+const Buttons = styled.div`
+  width: 20%;
+  text-align: right;
+  > button {
+    border: none;
+    border-radius: 5px;
+    margin-right: 10px;
+    padding: 5px 10px;
+  }
+`;
