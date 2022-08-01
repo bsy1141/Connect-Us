c422442a94e29b6e5b12318e116780140ec54748
bsy1141 <bsy1141@naver.com>
2022-08-02 01:32:04 +0900

    feat: 입학년월 졸업년월 자동형식 변환 & 형식 테스트 코드 추가

--- front/src/pages/user/mypage/portfolio/education/EducationAddForm.js
+++ front/src/pages/user/mypage/portfolio/education/EducationAddForm.js
@@ -16,8 +16,32 @@ const EducationAddForm = ({ userId, setIsAdding, setEducations }) => {
     school.length > 0 &&
     major.length > 0 &&
     position.length > 0 &&
-    admission.length > 0 &&
-    graduate.length > 0;
+    RegExp(/^\d{4}.(0[1-9]|1[012])$/).test(admission) &&
+    RegExp(/^\d{4}.(0[1-9]|1[012])$/).test(graduate);
+
+  const handleAdmissionChange = (e) => {
+    const value = e.target.value;
+    if (value.length > 7) return;
+
+    setAdmission(
+      value
+        .replace(/[^0-9]/g, "")
+        .replace(/^(\d{0,4})(\d{0,2})$/g, "$1.$2")
+        .replace(/(\.{0,1})$/g, "")
+    );
+  };
+
+  const handleGraduateChange = (e) => {
+    const value = e.target.value;
+    if (value.length > 7) return;
+
+    setGraduate(
+      value
+        .replace(/[^0-9]/g, "")
+        .replace(/^(\d{0,4})(\d{0,2})$/g, "$1.$2")
+        .replace(/(\.{0,1})$/g, "")
+    );
+  };
 
   const handleSubmit = async () => {
     try {
@@ -50,7 +74,7 @@ const EducationAddForm = ({ userId, setIsAdding, setEducations }) => {
           label="입학년월"
           variant="outlined"
           value={admission}
-          onChange={(e) => setAdmission(e.target.value)}
+          onChange={handleAdmissionChange}
           placeholder="2018.03"
           style={{ width: "25%", marginRight: "1%" }}
         />
@@ -58,7 +82,7 @@ const EducationAddForm = ({ userId, setIsAdding, setEducations }) => {
           label="졸업년월"
           variant="outlined"
           value={graduate}
-          onChange={(e) => setGraduate(e.target.value)}
+          onChange={handleGraduateChange}
           placeholder="2022.02"
           style={{ width: "25%" }}
         />
