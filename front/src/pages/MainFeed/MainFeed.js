5b660b476fa69f1c851cef74643e752c96c3ce60
bsy1141 <bsy1141@naver.com>
2022-08-09 18:16:38 +0900

    fix: useLayoutEffect->useEffect

--- front/src/pages/MainFeed/MainFeed.js
+++ front/src/pages/MainFeed/MainFeed.js
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect, useLayoutEffect, useState } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faPlus } from "@fortawesome/free-solid-svg-icons";
@@ -22,7 +22,7 @@ function MainFeed() {
     setPosts(res.data);
   };
 
-  useLayoutEffect(() => {
+  useEffect(() => {
     if (user) {
       setIsModalOpen(user.keywords.length === 0);
       setFollowing(user.followings);
