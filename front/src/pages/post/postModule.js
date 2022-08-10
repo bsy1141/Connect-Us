c1024224727b81a8f6d491e2d70ae94ee725ee14
bsy1141 <bsy1141@naver.com>
2022-08-10 23:57:15 +0900

    docs: 불필요한 목데이터 제거

--- front/src/pages/post/postModule.js
+++ front/src/pages/post/postModule.js
@@ -6,21 +6,3 @@ export const formatDate = (date) => {
 
   return `${year}년 ${month}월 ${day}일`;
 };
-
-export const fakeComments = [
-  {
-    authorName: "정혜정",
-    authorId: "0d2b5c22-8ab8-44c6-91e2-3f20270c7dd2",
-    text: "흥미로운 정보 감사합니다!",
-  },
-  {
-    authorName: "connectUs",
-    authorId: "8f2156f9-f53e-43c4-b79d-e5e7bc1f35fe",
-    text: "채용 제안하고 싶습니다. 연락주세요. connectUs@naver.com",
-  },
-  {
-    authorName: "정혜정",
-    authorId: "0d2b5c22-8ab8-44c6-91e2-3f20270c7dd2",
-    text: "팔로우 신청하고 갑니다!",
-  },
-];
