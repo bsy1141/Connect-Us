25b2ecb3ea6ed559e519b5986eb069e88d5cb66b
bsy1141 <bsy1141@naver.com>
2022-08-11 14:53:21 +0900

    feat: 포스트 사진 업로드 라우터 API 구현 완료

--- back/src/routers/uploadRouter.js
+++ back/src/routers/uploadRouter.js
@@ -1,29 +1,20 @@
-import is from "@sindresorhus/is";
+//import is from "@sindresorhus/is";
 import { Router } from "express";
-import multer from "multer";
+//import multer from "multer";
 import { login_required } from "../middlewares/login_required";
 
+const { postImgUpload } = require("../utils/s3");
+
 const uploadRouter = Router();
-uploadRouter.use(login_required);
-
-const storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, "uploads/");
-  },
-  filename: function (req, file, cb) {
-    cb(null, `${file.fieldname}_${Date.now()}_${file.originalname}`);
-  },
-});
-
-const upload = multer({ storage: storage });
 
 uploadRouter.post(
   "/uploadImage",
-  upload.single("image"),
+  login_required,
+  postImgUpload.single("image"),
   async function (req, res, next) {
     try {
-      const saveFileName = req.file.filename; // 저장된 파일명​
-      const saveFilePath = `http://localhost:5001/uploads/${saveFileName}`; // 업로드된 파일의 경로 (index.html 기준)
+      console.log(req.file);
+      const saveFilePath = req.file?.location ?? null; // 업로드된 파일의 경로 (index.html 기준)
 
       res.status(201).send(saveFilePath);
     } catch (error) {
