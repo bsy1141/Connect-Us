f4086f220b595c7d0f6db683c13450e6a103689e
bsy1141 <bsy1141@naver.com>
2022-09-09 23:04:29 +0900

    feat: 추천 결과 페이지 구현

--- front/src/pages/recommend/RecommendResultPage.js
+++ front/src/pages/recommend/RecommendResultPage.js
@@ -1,34 +1,63 @@
 import { useLocation } from "react-router-dom";
 import styled from "styled-components";
 import Header from "components/Header";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+import axios from "axios";
+import LoadingSpinner from "components/LoadingSpinner";
+import UserCard from "components/UserCard";
 
-const keywordTypeEng = {
-  직무: "job",
-  "직무 상세": "jobDetail",
-  근무지역: "workPlace",
-  경력: "career",
-  학력: "education",
-  고용형태: "employ",
-};
-
-const userTypeEng = {
-  전체: "all",
-  구직자: "user",
-  구인자: "company",
-};
+const serverUrl = "http://localhost:5001";
 
 const RecommentResultPage = () => {
   const {
     state: { userType, keywordType },
   } = useLocation();
+  const [loading, setLoading] = useState(false);
+  const [users, setUsers] = useState([]);
 
-  useEffect(() => {}, []);
+  const getRecommendUsers = async () => {
+    try {
+      const res = await axios.get(`${serverUrl}/recommend`, {
+        params: { userType, keywordType: keywordType.join(",") },
+        headers: {
+          Authorization: `Bearer ${sessionStorage.getItem("userToken")}`,
+        },
+      });
+      console.log(res.data);
+      setUsers(res.data);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+  useEffect(() => {
+    setLoading(true);
+    getRecommendUsers();
+    setLoading(false);
+  }, []);
+
+  if (loading) {
+    return (
+      <Container>
+        <Header />
+        <LoadingWrapper>
+          <LoadingSpinner />
+        </LoadingWrapper>
+      </Container>
+    );
+  }
 
   return (
     <Container>
       <Header />
-      <Content></Content>
+      <Content>
+        <h5>추천 결과</h5>
+        <p>우선순위 일치도가 높은 순서대로 결과가 보여집니다.</p>
+        <UserCardWrapper>
+          {users.map((user) => (
+            <UserCard key={user.id} user={user} />
+          ))}
+        </UserCardWrapper>
+      </Content>
     </Container>
   );
 };
@@ -42,9 +71,27 @@ const Container = styled.div`
   overflow-y: hidden;
 `;
 
+const LoadingWrapper = styled.div`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
 const Content = styled.div`
   width: 100%;
   height: 100%;
-  padding: 80px 3% 0;
-  background: #efefef;
+  padding: 90px 3% 0;
+  > p {
+    color: #7b7b7b;
+  }
+  overflow-y: scroll;
+`;
+
+const UserCardWrapper = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  margin-top: 40px;
+  padding-left: 40px;
 `;
